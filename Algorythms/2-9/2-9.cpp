29e36da7810d8170cef3f0769a5c72e539772297
blzz4c <blazego4c@gmail.com>
2022-11-12 17:52:10 +0500

    2-3Png

--- Algorythms/2-9/2-9.cpp
+++ Algorythms/2-9/2-9.cpp
@@ -32,5 +32,46 @@ int main()
         cout << "*" << endl;
     }
     cout << endl;
-
+     for (int i = 0; i < n; i++)
+    {
+        cout << "* ";
+    }
+    for (int i = 0; i < d - 2; i++)
+    {
+        cout << " ";
+    }
+    cout << "*" << endl;
+    for (int i = 0; i < n - d - 1; i++)
+    {
+        cout << "* ";
+        for (int j = 0; j < n-2; j++)
+        {
+            cout << " ";
+        }
+        cout << "* ";
+        for (int j = 0; j < d - 2; j++)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+    for (int i = 0; i < d-1; i++)
+    {
+        cout << "* ";
+        for (int j = 0; j < n-2; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        for (int j = 1; j < d-i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
 }
