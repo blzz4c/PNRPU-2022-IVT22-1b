7fd5aa2a407a152f18ba89caebe357230d35ca4a
blzz4c <blazego4c@gmail.com>
2022-11-22 20:28:56 +0500

    update

---
+++ Algorythms/2-23/2-23.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n, max = -100, num = 0;
+	bool flag = false;
+	cout << "Введите количество элементов n" << endl;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (max < sin(n + (i / n)))
+		{
+			max = sin(n + (i / n));
+		}
+	}
+    for (int i = 1; i <= n; i++)
+	{
+		if (max == sin(n + (i / n)))
+		{
+			num++;
+		}
+	}
+	cout << " Num: " << num;
+}
