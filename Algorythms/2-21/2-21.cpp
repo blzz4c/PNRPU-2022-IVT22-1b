65dcb6200037f4d94951f89c8e2b504996cd0432
blzz4c <blazego4c@gmail.com>
2022-11-22 20:12:59 +0500

    update

---
+++ Algorythms/2-21/2-21.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n1, n2;
+	bool Up = false;
+	bool Izm = false; //проверка изменчивости
+	cout << "Проверка чисел по возрастанию. 0 - признак окончания ввода" << endl;
+	cout << "Введите число: ";
+	cin >> n1;
+	n2 = n1;
+	while (n1 != 0)
+	{
+		cin >> n1;
+		if (n1 >= n2)
+		{
+			Up = true;
+		}
+		if (n1 < n2 && n1 != 0)
+		{
+			Up = false;
+			Izm = true;
+		}
+		n2 = n1;
+	}
+	if (Up && !Izm)
+	{
+		cout << "Числа расположены в порядке возрастания" << endl;
+	}
+	else
+	{
+		cout << "Числа расположены не в порядке возрастания" << endl;
+	}
+}
