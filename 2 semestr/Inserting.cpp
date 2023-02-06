33a54b12204a0515017462ea01ff266dfc99b01b
blzz4c <blazego4c@gmail.com>
2023-02-06 14:47:38 +0500

    new commit

--- 2 semestr/Inserting.cpp
+++ 2 semestr/Inserting.cpp
@@ -7,7 +7,6 @@ int main()
 	int arr[] = { 41, 42, 66, 87, 86, 38, 77, 84, 45, 35, 22 };
 	for (int i = 0; i < N; i++)
 	{
-		int min = arr[i];
 		for (int j = i; j > 0 && arr[j-1] > arr[j]; j--)
 		{
 			temp = arr[j];
