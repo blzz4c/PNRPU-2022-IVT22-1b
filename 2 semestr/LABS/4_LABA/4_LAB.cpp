28eeef82f78afa07026d962743399e6078f08ed9
blzz4c <blazego4c@gmail.com>
2023-02-08 15:51:07 +0500

    4_LAB

---
+++ 2 semestr/LABS/4_LABA/4_LAB.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int const SIZE = 10;
+
+	int answ, temp, num;
+
+	int arr[SIZE] = {};
+	
+	while(true)
+	{
+		std::cout << "Что вы хотите сделать?\n1.Добавить число \n2.Удалить число ";
+		std::cin >> answ;
+		if (answ == 1)
+		{
+			std::cout << "Введите число в стек: ";
+			std::cin >> num;
+			for (int i = 0; i < SIZE-1; i++)
+			{
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+			arr[SIZE - 1] = num;
+			for (int i = 0; i < SIZE; i++)
+			{
+				std::cout << arr[i] << " ";
+			}
+		}
+		else if(answ ==2)
+		{
+			for (int i = SIZE - 1; i > 0; i--)
+			{
+				temp = arr[i];
+				arr[i] = arr[i-1];
+				arr[i - 1] = temp;
+			}
+			arr[0] = 0;
+			for (int i = 0; i < SIZE; i++)
+			{
+				std::cout << arr[i] << " ";
+			}
+		}
+	}
+}
