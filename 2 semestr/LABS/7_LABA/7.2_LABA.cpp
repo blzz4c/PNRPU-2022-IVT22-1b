ddf5967edcd05103fd59a80b9a9b160fa3424f2b
blzz4c <blazego4c@gmail.com>
2023-02-26 02:37:14 +0500

    7-2

---
+++ 2 semestr/LABS/7_LABA/7.2_LABA.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+void SS(int k, ...)
+{
+	int *ptr = &k; //настроили указатель на параметр k
+	for (; k != 0; k--)
+	{
+		int num = *(++ptr);
+		int DEC = 0, i = 0;
+		while(num)
+		{
+			DEC += (num % 10)*pow(8, i);
+			num /= 10;
+			i++;
+		}
+		std::cout << DEC << " ";
+	}
+}
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	std::cout << "\nИз 8-ой СС в 10-ую(3, 8, 6, 11) = ";
+	SS(3, 8, 6, 11); //переводит в 10-ную систему счисления
+	std::cout << "\nИз 8-ой СС в 10-ую(4, 21, 233, 3, 12) = ";
+	SS(4, 21, 233, 3, 12);
+}
