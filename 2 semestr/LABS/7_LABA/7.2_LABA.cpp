82cded2f9f8641385d56c9c67ffb0c5ecef19f55
blzz4c <blazego4c@gmail.com>
2023-02-26 03:02:55 +0500

    красота 2.0

--- 2 semestr/LABS/7_LABA/7.2_LABA.cpp
+++ 2 semestr/LABS/7_LABA/7.2_LABA.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cmath>
 
-void SS(int k, ...){
+void SS(int k, ...) {
 	int *ptr = &k; //настроили указатель на параметр k
-	for (; k != 0; k--){
+	for (; k != 0; k--) {
 		int num = *(++ptr);
 		int DEC = 0, i = 0;
-		while(num){
-			DEC += (num % 10)*pow(8, i);
+		while(num) {
+			DEC += (num % 10) * pow(8, i);
 			num /= 10;
 			i++;
 		}
@@ -15,7 +15,7 @@ void SS(int k, ...){
 	}
 }
 
-int main(){
+int main() {
 	setlocale(LC_ALL, "Rus");
 	std::cout << "\nИз 8-ой СС в 10-ую(3, 8, 6, 11) = ";
 	SS(3, 8, 6, 11); //переводит в 10-ную систему счисления
