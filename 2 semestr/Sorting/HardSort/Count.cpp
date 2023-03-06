7aa2f668e872a039fbdb539613c3745ee0cdae73
blzz4c <blazego4c@gmail.com>
2023-03-06 12:20:24 +0500

    хыхыхыхы

--- 2 semestr/Sorting/HardSort/Count.cpp
+++ 2 semestr/Sorting/HardSort/Count.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 void countSort(int arr[], int size)
 {
-    int *output = new int[size];
-    int *count;
+    int* output = new int[size];
+    int* count;
     int max = arr[0];
 
     for (int i = 1; i < size; i++)
     {
-        if (arr[i] > max) { max = arr[i]; }
+        if (arr[i] > max) 
+        { 
+            max = arr[i]; 
+        }
     }
 
-    count = new int[max+1];
+    count = new int[max + 1];
     for (int i = 0; i <= max; ++i)
     {
         count[i] = 0;
@@ -21,10 +25,10 @@ void countSort(int arr[], int size)
     {
         count[arr[i]]++;
     }
-    
+
     for (int i = 1; i <= max; i++)
     {
-        count[i] += count[i-1];
+        count[i] += count[i - 1];
     }
     for (int i = size - 1; i >= 0; i--)
     {
@@ -33,6 +37,39 @@ void countSort(int arr[], int size)
     }
     for (int i = 0; i < size; i++)
     {
-        arr[i] = output[i]; //in-place сортировку делаем
+        arr[i] = output[i]; 
     }
+    delete[] count;
+    delete[] output;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    srand(time(NULL));
+    int size;
+    cout << "Введите размер массива: " << endl;
+    cin >> size;
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; i++) 
+    {
+        arr[i] = rand() % 101;
+    }
+    cout << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    countSort(arr, size);
+
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
+    return 0;
 }
