5950d6125f9f2b898391053ba4a7b796b2785418
blzz4c <blazego4c@gmail.com>
2023-03-06 11:21:36 +0500

    Перестановка сложных сортировок в подпапку и Shell

---
+++ 2 semestr/Sorting/HardSort/Shell.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int n = 10;
+	int m[n] = { 45, 96, 73, 43, 64, 12, 90, 88, 23, 45 };
+	int l = n / 2;
+	for (int l = n / 2; l > 0; l /= 2) {
+		for (int i = 0; i < n - l; i++) {
+			int j = i;
+			for (int j = i; j >= 0 && m[j] > m[j + l]; j--) {
+				int c = m[j];
+				m[j] = m[j + l];
+				m[j + l] = c;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << m[i] << ' ';
+	}
+}
