1ecabfb16b7e52454277ff124806967a03522817
blzz4c <blazego4c@gmail.com>
2023-03-28 08:41:04 +0500

    11.1

---
+++ Semester_2/11. 11.1 Однонаправленные списки через структуры/11.1_LABA.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+using namespace std;
+
+struct ListElem
+{
+    string data;
+    ListElem* next;
+};
+
+ListElem* createList(int n)
+{
+    ListElem* start;
+    ListElem* prev, * curr;
+    start = new ListElem;
+    cin >> start->data;
+    start->next = nullptr;
+    prev = start;
+    for (int i = 0; i < n - 1; i++)
+    {
+        curr = new ListElem;
+        cin >> curr->data;
+        curr->next = nullptr;
+        prev->next = curr;
+        prev = curr;
+    }
+    return start;
+}
+
+void delElems(ListElem* start, string element, int& size) { //correct
+    ListElem* curr = start;
+    ListElem* temp;
+    int pos = 0;
+    while (curr->data != element && curr->next != nullptr)
+    {
+        curr = curr->next;
+        pos++;
+    }
+    if (curr->data == element) {
+        if (pos == size - 1) {
+            curr = start;
+            for (int i = 0; i < pos - 2; i++)
+            {
+                curr = curr->next;
+            }
+            temp = curr->next->next;
+            curr->next = temp;
+            size--;
+        }
+        else if (pos == 0) {
+            temp = curr->next->next;
+            curr->next = temp;
+            size--;
+        }
+        else {
+            temp = curr->next->next;
+            curr->next = temp;
+            curr = start;
+            for (int i = 0; i < pos - 2; i++)
+            {
+                curr = curr->next;
+            }
+            temp = curr->next->next;
+            curr->next = temp;
+            size -= 2;
+        }
+    }
+    else {
+        cout << "Искомый элемент не найден.\n";
+    }
+}
+
+void addLElems(ListElem*& start, int k, int& size) {
+    for (int i = 1; i < k+1; i++) {
+        size++;
+        ListElem* first = start;
+
+        for (int j = 1; j < i-1; j++) {
+            first = first->next;
+        }
+
+        ListElem* new_element = new ListElem;
+        cout << "Введите элемент: ";
+        cin >> new_element->data;
+
+        if (i == 1) {
+            new_element->next = first;
+            start = new_element;
+        }
+        else {
+            new_element->next = first->next;
+            first->next = new_element;
+        }
+    }
+}
+void addRElems(ListElem* start, int k, int &size) {
+    ListElem* curr = start;
+    for (int i = 0; i < k; i++) {
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+        size++;
+        ListElem* new_element = new ListElem;
+        cout << "Введите элемент: ";
+        cin >> new_element->data;
+        new_element->next = curr->next;
+        curr->next = new_element;
+    }
+}
+
+void printList(ListElem* first, int n) {
+    ListElem* curr = first;
+    for (int i = 0; i < n; i++) {
+        cout << curr->data << ' ';
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+void deleteList(ListElem* list) {
+    delete list;
+}
+
+int main() {
+    system("chcp 1251>NULL");
+    int size, k, pos;
+    string element;
+    cout << "Введите размер списка: ";
+    cin >> size;
+    cout << "Введите значения элементов в списке: ";
+    ListElem* list = createList(size);
+    cout << "Вывод списка\n";
+    printList(list, size);
+
+    cout << "Введите количество добавляемых элементов с левой стороны: ";
+    cin >> k;
+    addLElems(list, k, size);
+    cout << "Вывод списка\n";
+    printList(list, size);
+
+    cout << "Введите количество добавляемых элементов с правой стороны: ";
+    cin >> k;
+    addRElems(list, k, size);
+    cout << "Вывод списка\n";
+    printList(list, size);
+
+    cout << "Введите элемент, где хотите перед и после него удалить элементы: ";
+    cin >> element;
+    delElems(list, element, size);
+    cout << "Вывод списка\n";
+    printList(list, size);
+    deleteList(list);
+}
