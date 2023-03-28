3ab43c5e892498ffd5e50b7be48f1a973c161608
blzz4c <blazego4c@gmail.com>
2023-03-28 22:27:31 +0500

    KMP

--- Semester_2/19. Сложные поиски/KMP-BM.cpp
+++ Semester_2/19. Сложные поиски/KMP-BM.cpp
@@ -4,31 +4,6 @@ using namespace std;
 
 string sub, s;
 
-int KMP() {
-	int* t = new int[sub.length()] {};
-	int k = 0;
-	for (int i = 1; i < sub.length(); i++) {
-		while (k > 0 && sub[i] != sub[k]) {
-			k = t[k - 1];
-		}
-		if (sub[i] == sub[k]) {
-			k++;
-		}
-		t[i] = k;
-	}
-	k = 0;
-	for (int i = sub.length(); i < s.length(); i++) {
-		while (k > 0 && sub[k] != s[i]) {
-			k = t[k - 1];
-		}
-		k++;
-		if (k == sub.length()) {
-			return i - sub.length() + 1;
-		}
-	}
-	return -1;
-}
-
 int BM() {
     unsigned int shift[256];
     for (int i = 0; i < 256; i++) {
@@ -44,7 +19,7 @@ int BM() {
         bool find = true;
         for (int i = sub.length() - 1; i >= 0 && find; i--) {
             if (s[ei - sub.length() + 1 + i] != sub[i]) {
-                ei += shift[s[ei - sub.length() + 1 + i] +128];
+                ei += shift[s[ei - sub.length() + 1 + i] + 128];
                 find = false;
             }
         }
@@ -55,13 +30,45 @@ int BM() {
     return -1;
 }
 
+int KMP() {
+    int* t = new int[sub.length()] {};
+    int k = -1;
+    t[0] = -1;
+    int i = 0;
+    while(i < sub.length() - 1) {
+        while (k >= 0 && sub[i] != sub[k]) {
+            k = t[k];
+        }
+        i++;
+        k++;
+        if (sub[i] == sub[k]) {
+            t[i] = t[k];
+        }
+        else {
+            t[i] = k;
+        }
+    }
+    for (i = 0, k = 0; i < s.length() && k < sub.length(); i++, k++) {
+        while (k >= 0 && sub[k] != s[i]) {
+            k = t[k];
+        }
+    }
+    delete[] t;
+    if (k == sub.length()) {
+        return i - k;
+    }
+    else {
+        return -1;
+    }
+}
+
 int main() {
-	system("chcp 1251 > Null");
-	cout << "Введите строку, в которой требуется найти образ: ";
-	cin >> s;
-	cout << "Введите образ: ";
-	cin >> sub;
-	cout << BM();
-    //cout << KMP();
-	return 0;
+    system("chcp 1251 > Null");
+    cout << "Введите строку, в которой требуется найти образ: ";
+    cin >> s;
+    cout << "Введите образ: ";
+    cin >> sub;
+    //cout << BM();
+    cout << KMP();
+    return 0;
 }
