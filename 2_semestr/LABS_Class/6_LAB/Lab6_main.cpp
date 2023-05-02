4f48b068802040ace6a6666b0847826a034a66b8
blzz4c <blazego4c@gmail.com>
2023-05-02 18:29:50 +0500

    fix

--- 2_semestr/LABS_Class/6_LAB/Lab6_main.cpp
+++ 2_semestr/LABS_Class/6_LAB/Lab6_main.cpp
@@ -5,14 +5,19 @@ using namespace std;
 void main() {
 	Set a(5);
 	cout << a << "\n";
+
 	cin >> a;
 	cout << a << "\n";
+
 	a[2] = 100;
 	cout << a << "\n";
+
 	Set b(5);
 	cout << b << "\n";
+
 	b = a;
 	cout << b << "\n";
+
 	//!= - проверка на неравенство
 	if (a != b) {
 		cout << "true\n";
@@ -20,6 +25,7 @@ void main() {
 	else {
 		cout << "false\n";
 	}
+
 	//< число - принадлежность множеству
 	int n = 10;
 	if (a < n) {
@@ -28,12 +34,16 @@ void main() {
 	else {
 		cout << "false\n";
 	}
+
 	cout << *(a.first()) << "\n";
+
 	Iterator i = a.first();
 	++i;
 	cout << *i << "\n";
+
 	i + 2;
 	cout << *i << "\n";
+    
 	for (i = a.first(); i != a.last(); ++i) {
 		cout << *i << " ";
 	}
