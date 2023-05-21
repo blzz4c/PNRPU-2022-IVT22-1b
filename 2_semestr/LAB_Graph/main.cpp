2e89c520b0fa301c837f014f44675f8505adf3eb
blzz4c <blazego4c@gmail.com>
2023-05-22 01:39:14 +0500

    Graph

---
+++ 2_semestr/LAB_Graph/main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include "graph.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Graph w;
+    w.show();
+    return a.exec();
+}
