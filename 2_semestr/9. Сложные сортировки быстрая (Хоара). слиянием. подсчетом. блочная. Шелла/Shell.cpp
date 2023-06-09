2bd20eb0bfa9a5d5cead1fd0dae4265b9955e2e2
blzz4c <113666403+blzz4c@users.noreply.github.com>
2023-06-09 11:48:45 +0500

    Update Shell.cpp

--- 2_semestr/9. Сложные сортировки быстрая (Хоара). слиянием. подсчетом. блочная. Шелла/Shell.cpp
+++ 2_semestr/9. Сложные сортировки быстрая (Хоара). слиянием. подсчетом. блочная. Шелла/Shell.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
 	const int n = 10;
 	int m[n] = { 45, 96, 73, 43, 64, 12, 90, 88, 23, 45 };
-	int l = n / 2;
 	for (int l = n / 2; l > 0; l /= 2) {
 		for (int i = 0; i < n - l; i++) {
-			int j = i;
 			for (int j = i; j >= 0 && m[j] > m[j + l]; j--) {
 				int c = m[j];
 				m[j] = m[j + l];
